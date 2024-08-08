40428e3bbad36150393a9fa74cd2c2c62523e734
themuaz97 <muaz.rahman97@gmail.com>
2024-08-09 00:15:14 +0800

    change background images &  text changed to black

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30'>
 				<h1 className='text-3xl font-semibold text-center text-white'>
 					Login
-					<span className='text-blue-500'> Borak</span>
+					<span className='text-black'> Borak</span>
 				</h1>
 
 				<form>
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -22,9 +22,9 @@ const SignUp = () => {
 
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up <span className='text-blue-500'> Borak</span>
+					Sign Up <span className='text-black'> Borak</span>
 				</h1>
 
 				<form onSubmit={handleSubmitForm}>
